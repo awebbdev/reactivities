c128dd4e7e3a36ee38ccd33d891b57ef08f4ac88
Adam R Webb <adamwebb13@gmail.com>
2023-03-02 17:30:44 -0600

    Completed basic front end

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,11 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import './styles.css';
 import axios from 'axios';
-import { Container, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from './models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-
+import {v4 as uuid} from 'uuid';
 function App() {
 
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -36,6 +36,18 @@ function App() {
     setEditMode(false);
   }
 
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id 
+      ? setActivities([...activities.filter(a => a.id !== activity.id), activity])
+      : setActivities([...activities, {...activity, id: uuid()}]);
+      setEditMode(false);
+      setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter(a => a.id !== id)])
+  }
+
   return (
     <Fragment>
     
@@ -49,6 +61,8 @@ function App() {
           editMode={editMode}
           openForm={handleFormOpen}
           closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
         />
       </Container>
 
