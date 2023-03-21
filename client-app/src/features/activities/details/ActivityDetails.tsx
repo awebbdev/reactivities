80e6f489ed9356e599d4bab3f93b7b3528c4acb2
Adam R Webb <adamwebb13@gmail.com>
2023-03-21 16:18:02 -0500

    Added mobx state mgmt and refactored

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import { Activity } from '../../../app/layout/models/activity';
 import { Button, Card, Image } from 'semantic-ui-react';
+import { useStore } from '../../../app/stores/store';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
-interface Props {
-    activity: Activity | undefined;
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
-}
+export default function ActivityDetails( ) {
 
-export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
+    const {activityStore} = useStore();
+    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+
+    if(!activity) return <LoadingComponent />;
+
     return (
         <Card fluid>
-            <Image src={`/assets/categoryImages/${activity?.category}.jpg`} />
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
             <Card.Content>
-                <Card.Header>{activity?.title}</Card.Header>
+                <Card.Header>{activity.title}</Card.Header>
                 <Card.Meta>
-                    <span>{activity?.date}</span>
+                    <span>{activity.date}</span>
                 </Card.Meta>
                 <Card.Description>
-                    {activity?.description}
+                    {activity.description}
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths='2'>
                     <Button onClick={() => openForm(activity?.id ?? '')} basic color='blue' content='Edit' />
-                    <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel' />
+                    <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel' />
                 </Button.Group>
             </Card.Content>
         </Card>
