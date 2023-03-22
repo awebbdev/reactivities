08de7d7fc04697953f4033892a790d2c1e01f6b1
Adam R Webb <adamwebb13@gmail.com>
2023-03-22 11:34:04 -0500

    Completed initial routing work

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,19 +3,24 @@ import './styles.css';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
 
 function App() {
-  
-  return (
-    <Fragment>
-    
-      <NavBar />
-      <Container style={{marginTop: '7em'}}>
-        <Outlet />
-      </Container>
+  const location = useLocation();
 
-    </Fragment>
+  return (
+    <>
+      {location.pathname === '/' ? <HomePage /> : (
+        <>
+          <NavBar />
+          <Container style={{marginTop: '7em'}}>
+            <Outlet />
+          </Container>
+        </>
+      )}
+
+    </>
   );
 }
 
